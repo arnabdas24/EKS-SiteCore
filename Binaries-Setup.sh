cf8e5b58f2b7e49f0ccfda89dee10a9dac58d733
Arnab Das <arnabds991@gmail.com>
2023-04-28 07:04:49 +0000

    updated git username

--- Binaries-Setup.sh
+++ Binaries-Setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git config --global user.name "Arnab Das"
+git config --global user.email "arnabds991@gmail.com"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
